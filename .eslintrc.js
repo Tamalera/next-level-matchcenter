6b4e098a2b8b4e2c41c4a9fe6f3821ae644f8d91
Tamalera <florence.meier@alumni.ethz.ch>
2019-11-15 13:02:28 +0100

    Fix linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,23 @@
 module.exports = {
-    root: true,
-    env: {
-      browser: true,
-      node: true
-    },
-    parserOptions: {
-      parser: 'babel-eslint',
-      sourceType: 'module'
-    },
-    extends: [
-      'plugin:prettier/recommended',
-      'prettier',
-      'prettier/vue'
-    ],
-    plugins: [
-      'prettier'
-    ],
-    rules: {
-      'no-console': 'off',
-      'prettier/prettier': ['error', {
-        singleQuote: true,
-        trailingComma: 'es5',
-        semi: false,
-      }],
-    }
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    "prettier/vue",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
+  globals: {
+    $nuxt: true
+  },
+  parserOptions: {
+    parser: "babel-eslint"
   }
-  
+};
