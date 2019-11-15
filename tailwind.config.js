4daaa50228c59a3d3db24b6f572228852497c725
Lenhardt Laura <Laura.Lenhardt@swisscom.com>
2019-11-15 18:12:02 +0100

    Laura

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      display: ['Arial', 'sans-serif'],
+      body: ['Arial', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {},
