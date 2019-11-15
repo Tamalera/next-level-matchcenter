3a36ad966c6641064cc5b8836990a341023e002a
Tamalera <florence.meier@alumni.ethz.ch>
2019-11-15 16:16:29 +0100

    Linting

--- src/App.vue
+++ src/App.vue
@@ -11,29 +11,29 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState } from 'vuex'
 
 export default {
   computed: {
-    ...mapState(["socket"])
+    ...mapState(['socket']),
   },
 
   watch: {
     // Socket Event Worker
-    async "socket.events"() {
-      const event = await this.$store.dispatch("nextEvent");
+    async 'socket.events'() {
+      const event = await this.$store.dispatch('nextEvent')
       if (event) {
         switch (event.type) {
-          case "new-bet":
-            this.$store.dispatch("bets/newBet", event);
-            break;
+          case 'new-bet':
+            this.$store.dispatch('bets/newBet', event)
+            break
         }
 
-        this.$store.commit("REMOVE_EVENT", event);
+        this.$store.commit('REMOVE_EVENT', event)
       }
-    }
-  }
-};
+    },
+  },
+}
 </script>
 
 <style lang="scss"></style>
