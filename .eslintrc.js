6b1ebbc5236881da3130ba9c900db1634fd36b54
Tom Diggelmann <mail@tomdgm.ch>
2019-11-15 22:36:09 +0100

    Fix no-console not required for prod

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,8 @@ module.exports = {
   ],
   rules: {
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
     'prettier/prettier': [
       'error',
       {
