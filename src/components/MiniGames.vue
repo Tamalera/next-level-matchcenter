272d91790a80f79652db91934b82632fa2e98d6e
Vanessa Eberhard <vanessa.eberhard@swisscom.com>
2019-11-15 14:01:37 +0100

    create miniGames

---
+++ src/components/MiniGames.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <p>Test</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MiniGames',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+p {
+  color: red;
+}
+</style>
