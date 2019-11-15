c1c638a68f58238a10374793620edbad40cce34e
Vanessa Eberhard <vanessa.eberhard@swisscom.com>
2019-11-15 18:18:17 +0100

    Button

--- src/views/MiniGames.vue
+++ src/views/MiniGames.vue
@@ -1,10 +1,36 @@
 <template>
   <div>
-    <div class="bg-yellow-500 h-24 p-4 flex items-center">
+    <div class="text-semibold bg-yellow-500 h-32 p-4 flex items-center">
       <h1 class="text-5xl">Mini Games</h1>
     </div>
-    <Penalty v-if="false" />
-    <Corner v-if="true" />
+    <div v-if="action == ''" class="my-32 text-center">
+      <div class="mt-16">
+        <button
+          class="font-semibold text-3xl bg-yellow-500 rounded-lg w-2/3 h-32"
+          @click="action = 'penalty'"
+        >
+          Penalty
+        </button>
+      </div>
+      <div class="mt-16">
+        <button
+          class="font-semibold text-3xl bg-yellow-500 rounded-lg w-2/3 h-32"
+          @click="action = 'corner'"
+        >
+          Corner
+        </button>
+      </div>
+    </div>
+    <div v-if="action != ''" class="my-8 text-center">
+      <button
+        class="text-2xl bg-yellow-500 rounded-lg w-2/3 h-24"
+        @click="action = ''"
+      >
+        Back
+      </button>
+    </div>
+    <Penalty v-if="action == 'penalty'" />
+    <Corner v-if="action == 'corner'" />
   </div>
 </template>
 
@@ -17,6 +43,11 @@ export default {
   components: {
     Penalty,
     Corner,
+  },
+  data: function() {
+    return {
+      action: '',
+    }
   },
 }
 </script>
