4fa4d992a0853fa6fab8b0e6ea380438395cbc74
Tom Diggelmann <mail@tomdgm.ch>
2019-11-16 04:46:15 +0100

    Fix modal style

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,10 +3,12 @@
   <Transition name="fade">
     <div
       v-if="showing"
-      class="fixed inset-0 w-full h-screen flex items-center justify-center bg-semi-75"
+      class="fixed inset-0 w-full h-screen flex items-center justify-center bg-overlay"
       @click.self="close"
     >
-      <div class="relative w-full max-w-2xl bg-white shadow-lg rounded-lg p-8">
+      <div
+        class="relative w-full mx-6 max-w-2xl bg-white shadow-2xl rounded-lg p-8"
+      >
         <button
           aria-label="close"
           class="absolute top-0 right-0 text-xl text-gray-500 my-2 mx-4"
@@ -37,6 +39,10 @@ export default {
 </script>
 
 <style scoped>
+.bg-overlay {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
 .fade-enter-active,
 .fade-leave-active {
   transition: all 0.4s;
