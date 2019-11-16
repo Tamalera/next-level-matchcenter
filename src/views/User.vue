b6d400fa42d9b751b1dc7facaa252480d28ceca3
Lenhardt Laura <Laura.Lenhardt@swisscom.com>
2019-11-16 02:07:34 +0100

    add User view

---
+++ src/views/User.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="user w-full pb-16">
+    <div
+      class="h-screen pt-8 flex flex-col items-center bg-no-repeat bg-center bg-cover shadow-inner"
+      :style="{
+        backgroundImage: 'url(/images/stadion.jpg)',
+      }"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
